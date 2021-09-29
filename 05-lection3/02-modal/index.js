9f3acba2ecedfa8cf93c36769d6e87dffb891fdb
Alexander Prunyak <987456987p@gmail.com>
2021-09-29 14:25:48 +0300

    add solution for 05_lection3_02_modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,18 +2,29 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const modalWindow = document.querySelector('.modal__window');
 
     if (!close || !modalOpen || !modal) {
         return;
     }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+    modalOpen.addEventListener('click', openModal);
 
-    close.addEventListener('click', () => {
+    close.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', closeModal);
+
+    modalWindow.addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
+
+    function closeModal() {
         modal.style.display = 'none';
         document.body.overflow = 'initial';
-    });
+    }
+
+    function openModal() {
+        modal.style.display = 'block';
+        document.body.overflow = 'hidden';
+    }
 })();
